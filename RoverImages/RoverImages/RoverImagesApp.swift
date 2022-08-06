34bbc12681d034645c8fcef4b9a73edf309a9e34
developer <developer@developers-air.lan>
2022-08-05 20:11:27 -0400

    added starter project

---
+++ RoverImages/RoverImages/RoverImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoverImagesApp.swift
+//  RoverImages
+//
+//  Created by developer on 8/5/22.
+//
+
+import SwiftUI
+
+@main
+struct RoverImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
